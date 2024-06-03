d2bf1fb6546e69dc0ea549633ca2a245093d43d6
Heberth López <heberth.lopez.19@gmail.com>
2024-06-02 20:06:13 -0500

    Obtaining data from a REST API

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -3,6 +3,7 @@ import { ProductComponent } from '../../components/product/product.component';
 import { Product } from 'app/domains/shared/models/product.model';
 import { HeaderComponent } from 'app/domains/shared/components/header/header.component';
 import { CartService } from 'app/domains/shared/services/cart.service';
+import { ProductService } from 'app/domains/shared/services/product.service';
 
 @Component({
   selector: 'app-list',
@@ -14,60 +15,24 @@ import { CartService } from 'app/domains/shared/services/cart.service';
 export class ListComponent {
   products = signal<Product[]>([]);
   private cartService = inject(CartService);
+  private productService = inject(ProductService);
 
-  constructor() {
-    const initProducts: Product[] = [
-      {
-        id: Date.now(),
-        title: 'Product 1',
-        price: 700,
-        imageUrl: 'https://picsum.photos/640/640?r=24',
-        createdAt: new Date().toISOString(),
+  constructor() {}
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe({
+      next: (productsData) => {
+        const products = productsData.map((product) => {
+          if (product.images.length > 0 && product.images[0].startsWith('["')) {
+            product.images = product.images.map((img) => img.replace(/^\["|"|\]$/g, ''));
+          }
+          return product;
+        });
+        this.products.set(products);
       },
-      {
-        id: Date.now() + 1,
-        title: 'Product 2',
-        price: 679,
-        imageUrl: 'https://picsum.photos/640/640?r=34',
-        createdAt: new Date().toISOString(),
-      },
-      {
-        id: Date.now() + 2,
-        title: 'Product 3',
-        price: 689,
-        imageUrl: 'https://picsum.photos/640/640?r=36',
-        createdAt: new Date().toISOString(),
-      },
-      {
-        id: Date.now() + 3,
-        title: 'Product 4',
-        price: 699,
-        imageUrl: 'https://picsum.photos/640/640?r=39',
-        createdAt: new Date().toISOString(),
-      },
-      {
-        id: Date.now() + 4,
-        title: 'Product 5',
-        price: 499,
-        imageUrl: 'https://picsum.photos/640/640?r=45',
-        createdAt: new Date().toISOString(),
-      },
-      {
-        id: Date.now() + 5,
-        title: 'Product 6',
-        price: 599,
-        imageUrl: 'https://picsum.photos/640/640?r=48',
-        createdAt: new Date().toISOString(),
-      },
-      {
-        id: Date.now() + 6,
-        title: 'Product 7',
-        price: 777,
-        imageUrl: 'https://picsum.photos/640/640?r=57',
-        createdAt: new Date().toISOString(),
-      },
-    ];
-    this.products.set(initProducts);
+      error: (error) => console.error(error),
+      complete: () => console.log('Products loaded:', this.products()),
+    });
   }
 
   addToCart(product: Product) {
--- src/app/domains/shared/models/product.model.ts
+++ src/app/domains/shared/models/product.model.ts
@@ -2,6 +2,6 @@ export interface Product {
   id: number;
   title: string;
   price: number;
-  imageUrl: string;
+  images: string[];
   createdAt: string;
 }
