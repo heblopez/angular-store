50814ec7f05e95b2568f95ed4cfa8810502448d4
Heberth López <heberth.lopez.19@gmail.com>
2024-06-05 18:40:05 -0500

    Nested views

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,17 @@ import { Routes } from '@angular/router';
 import { ListComponent } from './domains/products/pages/list/list.component';
 import { AboutComponent } from './domains/info/pages/about/about.component';
 import { NotFoundComponent } from './domains/info/pages/not-found/not-found.component';
+import { LayoutComponent } from './domains/shared/components/layout/layout.component';
 
 export const routes: Routes = [
-  { path: '', component: ListComponent },
-  { path: 'about', component: AboutComponent },
-  { path: '**', component: NotFoundComponent }
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: ListComponent },
+      { path: 'about', component: AboutComponent },
+    ],
+  },
+  // { path: '', component: ListComponent },
+  { path: '**', component: NotFoundComponent },
 ];
