9c60cbc7e0b00bda64421b06b8fbc8bff340bd15
Heberth López <heberth.lopez.19@gmail.com>
2024-06-05 01:28:57 -0500

    404 Page and updated product card styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './domains/products/pages/list/list.component';
 import { AboutComponent } from './domains/info/pages/about/about.component';
+import { NotFoundComponent } from './domains/info/pages/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: ListComponent },
   { path: 'about', component: AboutComponent },
+  { path: '**', component: NotFoundComponent }
 ];
