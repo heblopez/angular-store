0d9a4cac2922f65a322b296a2fbb512bac4786c7
Heberth López <heberth.lopez.19@gmail.com>
2024-06-05 18:57:07 -0500

    Detail of each product

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { ListComponent } from './domains/products/pages/list/list.component';
 import { AboutComponent } from './domains/info/pages/about/about.component';
 import { NotFoundComponent } from './domains/info/pages/not-found/not-found.component';
 import { LayoutComponent } from './domains/shared/components/layout/layout.component';
+import { ProductDetailComponent } from './domains/products/pages/product-detail/product-detail.component';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,7 @@ export const routes: Routes = [
     children: [
       { path: '', component: ListComponent },
       { path: 'about', component: AboutComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
     ],
   },
   { path: '**', component: NotFoundComponent },
--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,12 +1,13 @@
 import { TimeAgoPipe } from '@/app/domains/shared/pipes/time-ago.pipe';
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Product } from '@shared/models/product.model';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CommonModule, TimeAgoPipe],
+  imports: [CommonModule, TimeAgoPipe, RouterLink],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
