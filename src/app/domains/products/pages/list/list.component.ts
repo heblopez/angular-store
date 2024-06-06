db08c97a552e85c811c43a7c70d1d4bce650314f
Heberth López <heberth.lopez.19@gmail.com>
2024-06-06 17:18:39 -0500

    Url Params

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
 import { ProductComponent } from '@products/components/product/product.component';
 import { Product } from '@shared/models/product.model';
 import { HeaderComponent } from '@shared/components/header/header.component';
@@ -6,11 +6,12 @@ import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
 import { CategoryService } from '@/app/domains/shared/services/category.service';
 import { Category } from '@/app/domains/shared/models/category.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent, HeaderComponent],
+  imports: [ProductComponent, HeaderComponent, RouterLink],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
@@ -20,12 +21,20 @@ export class ListComponent {
   private cartService = inject(CartService);
   private productService = inject(ProductService);
   private categoryService = inject(CategoryService);
+  @Input() category_id?: string;
 
   constructor() {}
 
   ngOnInit() {
     this.getProducts();
     this.getCategories();
+  }
+
+  ngOnChanges(changes: SimpleChanges){    
+    const categoryId = changes['category_id']
+    if (categoryId) {
+      this.getProducts()
+    }
   }
 
   addToCart(product: Product) {
@@ -45,7 +54,7 @@ export class ListComponent {
   }
 
   private getProducts() {
-    this.productService.getProducts().subscribe({
+    this.productService.getProducts(this.category_id).subscribe({
       next: (productsData) => {
         const products = this.sanitizeImagesUrl(productsData);
         this.products.set(products);
--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -9,8 +9,12 @@ export class ProductService {
   private http = inject(HttpClient);
   constructor() {}
 
-  getProducts() {
-    return this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products');
+  getProducts(categoryId?: string) {
+    const url = new URL('https://api.escuelajs.co/api/v1/products');
+    if (categoryId) {
+      url.searchParams.set('categoryId',categoryId)
+    }
+    return this.http.get<Product[]>(url.toString());
   }
 
   getOneProduct(id: string) {
