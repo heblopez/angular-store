02164d1ce8f35ca779d7988313d7625fc7f5efa8
Heberth López <heberth.lopez.19@gmail.com>
2024-06-05 19:19:56 -0500

    Obtaining product data

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Product } from '@/app/domains/shared/models/product.model';
+import { ProductService } from '@/app/domains/shared/services/product.service';
+import { CommonModule } from '@angular/common';
+import { Component, inject, Input, signal } from '@angular/core';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.component.html',
-  styleUrl: './product-detail.component.css'
+  styleUrl: './product-detail.component.css',
 })
 export class ProductDetailComponent {
+  private productService = inject(ProductService);
+  @Input() id?: string;
+  product = signal<Product>({} as Product)
 
+  ngOnInit() {
+    if (this.id) {
+      this.productService.getOneProduct(this.id).subscribe({
+        next: (product) => this.product.set(product),
+      });
+    }
+  }
 }
